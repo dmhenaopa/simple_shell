329cdc31d6f1c4acc780588196223759a757858a
jeanpierreba <2713@holbertonschool.com>
2021-04-13 19:26:46 +0000

    Added header file

--- holberton.h
+++ holberton.h
@@ -5,18 +5,41 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <dirent.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
+#define separator ": "
+#define prompt ("($) ")
+#define DELIMITER_ " "
+#define DELIMITER ":"
 
-#define prompt ("($ )")
+/**
+* struct builtins - built-ins selector
+* @s: built-in we get from the user
+* @f: name of the function
+*/
 
+typedef struct builtins
+{
+	char *s;
+	int (*f)(void);
+} selector_t;
+
+char **allocate_memory(void);
+char *get_path(char **buffer, char **argv, int count);
+void tokenizator(char *line, char **argv, int count);
+int verify_access(char *full_path, char **buffer, char **argv, int count);
+int execute_function(char *full_path, char **buffer);
+char *_strdup(char *str);
 char *_strcat(char *dest, char *src);
-int verify_access(char *full_path, char *buffer);
-int execute_f(char *full_path, char *buffer);
-void tokenizator(char *buffer);
-char **get_path(char *buffer);
-char **mallocargument(void);
+char *_strcpy(char *dest, char *src);
+void remove_new_line(char **line, ssize_t size_line);
+int selector3000(char *string);
+int get_environment(void);
+int change_directory(char *str);
 int putchar_func(char character);
+int _strcmp(char *s1, char *s2);
+void controlador(int numeroSenhal);
+void _puts(char *str);
 
 #endif
