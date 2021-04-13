2232a9d2038b3d1e5bae475ae6841c04c8ae7541
Steven Gonzalez <stevengm45@gmail.com>
2021-04-13 20:41:31 +0000

    execve function

---
+++ execute_function.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *execute_function - function shell
+ *Description: run the shell
+ *@full_path: path complete
+ *@buffer: get the values of the line
+ *Return: 0
+ */
+
+int execute_function(char *full_path, char **buffer)
+{
+if (execve(full_path, buffer, NULL) == -1)
+{
+exit(EXIT_FAILURE);
+}
+return (0);
+}
