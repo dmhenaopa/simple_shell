ce3db63811d5b5934c2e7862f8541cad9813711c
jeanpierreba <2713@holbertonschool.com>
2021-04-13 19:46:03 +0000

    Added condition to fail in non-interactive mode

--- verify_access.c
+++ verify_access.c
@@ -1,30 +1,54 @@
 #include "holberton.h"
-#define DELIMITER ":"
 
 /**
 * verify_access - shell function
 * @full_path: absolute path to the executable command
 * @buffer: additional arguments
-*
+* @argv: arguments we pass to the program
+* @count: count of lines
 * Description: Verify if the executable command exists in
 * the full path
 * Return: 1 if command has founded in some of the directories of
 * PATH. Otherwise returns 0
 */
-int verify_access(char *full_path, char *buffer)
-{
-	printf("Aqui es verify_access");
-	int i, verify_access, access_flag;
 
-	access_flag = 0;
-	for (i = 0; full_path[i] != '\0'; i++)
+int verify_access(char *full_path, char **buffer, char **argv, int count)
+{
+	(void)argv;
+	int verify_access, access_flag = 0, status;
+	pid_t id_child = 0;
+
+	verify_access = access(full_path, F_OK);
+
+	if (verify_access == 0)
 	{
-		verify_access = access(full_path, F_OK);
-		if (verify_access == 0)
-		{
-			printf("Aqui es verify_access, el IF");
-			execute_f(full_path, buffer);
-		}
+	id_child = fork();
+	if (id_child == 0)
+		execute_function(full_path, buffer);
+	else if (id_child > 0)
+		wait(&status);
+	else
+		perror("Error");
 	}
+
+	else if (!(isatty(STDIN_FILENO)))
+	{
+		_puts(argv[0]);
+		_puts(separator);
+			if (count < 10)
+				putchar_func(count + '0');
+			else if (count >= 10)
+			{
+				putchar_func((count / 10) + '0');
+				putchar_func((count % 10) + '0');
+			}
+		_puts(separator);
+		_puts(*buffer);
+		_puts(": not found");
+		_puts("\n");
+	}
+	else
+	perror(argv[0]);
+
 	return (access_flag);
 }
