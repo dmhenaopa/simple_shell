3ea236d0e7844cc2b9a3a567a812b9b92719d9fa
jeanpierreba <2713@holbertonschool.com>
2021-04-14 20:49:47 +0000

    Added exit 127 in error

--- verify_access.c
+++ verify_access.c
@@ -19,7 +19,6 @@ int verify_access(char *full_path, char **buffer, char **argv, int count)
 	(void)argv;
 
 	access_flag = 0;
-
 	verify_access = access(full_path, F_OK);
 
 	if (verify_access == 0)
@@ -30,26 +29,25 @@ int verify_access(char *full_path, char **buffer, char **argv, int count)
 		else if (id_child > 0)
 			wait(&status);
 		else
-			perror("Error");
+			exit(127);
 	}
-
 	else if (!(isatty(STDIN_FILENO)))
 	{
 		_puts(argv[0]);
 		_puts(separator);
-			if (count < 10)
-				putchar_func(count + '0');
-			else if (count >= 10)
-			{
-				putchar_func((count / 10) + '0');
-				putchar_func((count % 10) + '0');
-			}
+		if (count < 10)
+			putchar_func(count + '0');
+		else if (count >= 10)
+		{
+			putchar_func((count / 10) + '0');
+			putchar_func((count % 10) + '0');
+		}
 		_puts(separator);
 		_puts(*buffer);
 		_puts(": not found");
 		_puts("\n");
+		exit(127);
 	}
-
 	else
 		access_flag = -1;
 
